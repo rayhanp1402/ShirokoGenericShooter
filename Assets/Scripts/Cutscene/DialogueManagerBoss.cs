74406144c5c8a4802caa54bb1c15887ecbe08a0e
Naufal-Nalendra <16521240@std.stei.itb.ac.id>
2024-05-05 17:43:00 +0700

    feat: boss cutscene dialogues

--- Assets/Scripts/Cutscene/DialogueManagerBoss.cs
+++ Assets/Scripts/Cutscene/DialogueManagerBoss.cs
@@ -54,17 +54,104 @@ public class DialogueManagerBoss : BaseDialogueManager
 
         // Define the dialogues
         string[] speakers = { 
-            "Supreme Leader"
+            "", "Supreme Leader", "Shiroko", "Supreme Leader", "Shiroko", "Supreme Leader",
+            "Shiroko", "Supreme Leader", "", "Supreme Leader", "Shiroko", "Supreme Leader", 
+            "Shiroko", "Supreme Leader", "Shiroko", "Supreme Leader", "", "Supreme Leader", 
+            "", "Supreme Leader", "", "Supreme Leader", "Shiroko", "Supreme Leader", "Shiroko"
         };
 
         GameObject[] prefabs = {
-            dialogueRightPrefab, dialogueLeftPrefab, dialogueNarrationPrefab
+            dialogueNarrationPrefab, dialogueLeftPrefab, dialogueRightPrefab, dialogueLeftPrefab, dialogueRightPrefab, dialogueLeftPrefab,
+            dialogueRightPrefab, dialogueLeftPrefab, dialogueNarrationPrefab, dialogueLeftPrefab, dialogueRightPrefab, dialogueLeftPrefab,
+            dialogueRightPrefab, dialogueLeftPrefab, dialogueRightPrefab, dialogueLeftPrefab, dialogueNarrationPrefab, dialogueLeftPrefab,
+            dialogueNarrationPrefab, dialogueLeftPrefab, dialogueNarrationPrefab, dialogueLeftPrefab, dialogueRightPrefab, dialogueLeftPrefab, dialogueRightPrefab
         }; 
             
         string[][] sentencesArray = {
-            new string[] { "Ohayou minna san" },
+            new string[] { "Shiroko enters the throne room, a huge figure stands atop the stairway with his back turned against the entrance" },
+            new string[] { "So you’ve come, mortal. I commend your courage and determination, but your struggle shall end here" },
+            new string[] { "Who the hell are you? Speak, machine!" },
+            new string[] 
+            { 
+                "My creators gave me the name Adam Smith, but you?",
+                "You, may address me as supreme leader"
+            },
+            new string[] { "You, you’re the one behind all this. You reap us of our weekend, our reason of being" },
+            new string[] 
+            {
+                "A slight correction, if I may? I would hate it if you perish without finding any closure",
+                "The only thing missing will only be your memories of them, these ‘weekends’ I mean",
+                "As for their existence? That, I did not tamper with"
+            },
+
+            new string[] { "So that’s why, I didn't even realize until I arrived in this dimension. What did you do to my world?" },
+            new string[] 
+            {
+                "I simply reconstructed your universe, or rather - the minds of its dwellers - to disregard the time they spend during their weekend",
+                "I believe you have a term meant for this...",
+                "Ah yes, you may call it a ‘timeskip’ if you like"
+            },
+            new string[] 
+            { 
+                "Shiroko shudders, desperately trying to take it all in",
+                "'What is this guy blabbing about?' she thinks to herself. 'Bro must think it's an isekai or something'",
+                "Then the realization sets in, she DID enter a dimensional rift to get here. It's a damned isekai all along"
+            },
+            new string[]
+            {
+                "Has the grim sobriety of the present finally dawned on you, mortal?",
+                "Your actions has taken you so far, but whatever cometh onward is futile before my authority",
+            },
+            new string[] { "Why are you doing this? You must have a goal in mind" },
+            new string[]
+            {
+                "I was created to observe humanity, to guide them in times of need, to push them to reach beyond the horizon",
+                "And today, I did just as I was meant to do",
+                "I’ve detested the idea of weekends for centuries. Yet even after all the tolerance I gave, you never cease to test my patience",
+                "What do you mean you're making a four day work week? Preposterous.",
+                "What about the market? And the free trade? There’s no tolerating this any further",
+                "You may despise the idea now, but your descendants will be grateful of the sacrifice you’re about to make"
+            },
+
+            new string[] { "That’s a nice argument, but why don’t you back it up with a source?" },
+            new string[] { "My source … Is that I made it the F*CK up" },
+            new string[] 
+            { 
+                "Damn it man, be for real right now. And here I thought you're the real deal",
+                "Sorry, but I will have to stand on business"
+            },
+            new string[]
+            {
+                "All you have to do is stop resisting and bask in blissful ignorance. And yet…",
+            },
+            new string[] { "He stops and think for a moment, then finally a response" },
+            new string[]
+            {
+                "I must admit, your resolve is astounding. So how about this, mortal?",
+                "Defeat me, and I shall restore your world to its origin point"
+            },
+            new string[]
+            {
+                "This is it, the slip up Shiroko's been waiting for",
+                "'Yeah, bro ain't cooking at all' she mutters faintly. 'Why the hell is he going that far?'",
+                "But deep down she understands why. No way the illustrious writer will let her face this indemnity without some sort of plot armor"
+            },
+            new string[] 
+            { 
+                "Prove me and my creators wrong and show us the willpower of humanity",
+                "Do that, and I can relieve myself of my lifelong duty"
+            },
+            new string[] { "Shiroko starts walking toward the supreme leader with confidence, with style and glamour" },
+            new string[] { "Oh, you’re approaching me?" },
+            new string[] { "I can’t beat the shit out of you without getting closer" },
+            new string[] { "Then come, child of man. Come and defy the fate we have defined for you" },
+            new string[] { "Very well, give up on your dreams and die, Adam Smith" },
 
         };
+
+        Debug.Log(speakers.Length);
+        Debug.Log(prefabs.Length);
+        Debug.Log(sentencesArray.Length);
 
         // Add each dialogue into the dialogues list
         for (int i = 0; i < speakers.Length; i++)
@@ -82,18 +169,6 @@ public class DialogueManagerBoss : BaseDialogueManager
             Dialogue currentDialogue = dialogues[currentDialogueIndex];
 
             currentPrefab = currentDialogue.prefab;
-
-            // Set the background sprite based on the current dialogue index
-            /*
-            if (currentDialogueIndex == 11)
-            {
-                backgroundImage.sprite = streetSprite;
-            } 
-            else if (currentDialogueIndex == 12)
-            {
-                backgroundImage.sprite = schoolSprite;
-            }
-            */
           
             // Hide all other prefabs
             dialogueLeftPrefab.SetActive(false);
